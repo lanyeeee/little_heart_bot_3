c5efced7c012de6c5da495808f8a129957dda5f5
lanyeeee <lanyeeeeee@gmail.com>
2022-07-18 09:52:09 +0800

    feat: 发送弹幕初步完成

--- src/main/App.cs
+++ src/main/App.cs
@@ -1,3 +1,4 @@
+using little_heart_bot_3.entity;
 using little_heart_bot_3.others;
 
 namespace little_heart_bot_3.main;
@@ -8,6 +9,7 @@ public class App
     public static App Instance => _instance ?? new App();
 
     private readonly Logger _logger;
+    private List<UserEntity> _users;
 
     private App()
     {
@@ -15,8 +17,21 @@ public class App
         _logger = new Logger("app");
     }
 
+    public async Task SendMessage()
+    {
+        foreach (var user in _users)
+        {
+            List<MessageEntity> messages = await Globals.MessageRepository.GetMessagesByUid(user.Uid);
+            foreach (var message in messages)
+            {
+                await message.Send(user.Cookie, user.Csrf, _logger);
+            }
+        }
+    }
+
 
     public async Task Main()
     {
+        _users = await Globals.UserRepository.GetIncompletedUsers(20);
     }
 }
--- src/repository/UserRepository.cs
+++ src/repository/UserRepository.cs
@@ -1,4 +1,5 @@
 using Dapper;
+using little_heart_bot_3.entity;
 using little_heart_bot_3.others;
 using MySqlConnector;
 
@@ -10,5 +11,19 @@ public class UserRepository
     {
         await using var conn = new MySqlConnection(Globals.ConnectionString);
         await conn.ExecuteAsync("update user_table set completed = 0, config_num = 0 where 1");
+    }
+
+    public async Task<List<UserEntity>> GetIncompletedUsers(int num)
+    {
+        await using var conn = new MySqlConnection(Globals.ConnectionString);
+        var parameters = new { Num = num };
+        var result = await conn.QueryAsync<UserEntity>("select * from user_table limit @Num", parameters);
+        return result.ToList();
+    }
+
+    public async Task MarkCookieError(string? uid)
+    {
+        await using var conn = new MySqlConnection(Globals.ConnectionString);
+        await conn.ExecuteAsync($"update user_table set cookie_status = -1 where uid = {uid}");
     }
 }
