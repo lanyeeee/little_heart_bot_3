627edefa6ff61a470df2624bb8b0241879ac5faa
lanyeeee <lanyeeeeee@gmail.com>
2022-07-18 14:46:39 +0800

    chore: 把一些string改成string?

--- src/entity/UserEntity.cs
+++ src/entity/UserEntity.cs
@@ -4,15 +4,15 @@ namespace little_heart_bot_3.entity;
 
 public class UserEntity
 {
-    public string Uid { get; set; }
-    public string Cookie { get; set; }
-    public string Csrf { get; set; }
+    public string? Uid { get; set; }
+    public string? Cookie { get; set; }
+    public string? Csrf { get; set; }
     public int Completed { get; set; }
     public int CookieStatus { get; set; }
     public int ConfigNum { get; set; }
     public int TargetNum { get; set; }
-    public string ReadTimestamp { get; set; }
-    public string ConfigTimestamp { get; set; }
+    public string? ReadTimestamp { get; set; }
+    public string? ConfigTimestamp { get; set; }
 
     public async Task SendMessage(Logger logger)
     {
