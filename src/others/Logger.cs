7555168f4c4aa6055dda108b6912899d699a7648
lanyeeee <1210347077@qq.com>
2023-03-05 20:58:33 +0800

    feat: 日志文件大小现在要到1MB才切换

--- src/others/Logger.cs
+++ src/others/Logger.cs
@@ -6,20 +6,21 @@ public class Logger
     private TextWriter _writer;
     private string _fileName;
     private readonly string _name;
+    private readonly int _maxSize;
     private int _count;
 
-    public Logger(string name)
+    public Logger(string name, int maxSize = 256 * 1024)
     {
         _name = name;
         _fileName = _name + "_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
-
+        _maxSize = maxSize;
         Directory.CreateDirectory("log");
         _writer = TextWriter.Synchronized(File.AppendText("log/" + _fileName));
     }
 
     public async Task Log(params object[] args)
     {
-        string text = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}]";
+        string text = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}]";
 
         foreach (var arg in args)
         {
@@ -43,7 +44,7 @@ public class Logger
 
     private bool NeedToRoll()
     {
-        return new FileInfo("log/" + _fileName).Length > 256 * 1024;
+        return new FileInfo("log/" + _fileName).Length > _maxSize;
     }
 
     private void Roll()
