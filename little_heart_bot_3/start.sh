d9fde0e48c6b9ab7fb9eb5430878eb3fdbc2ccf0
lanyeeee <1210347077@qq.com>
2023-12-08 10:40:44 +0800

    fix: 修正启动和终止脚本的路径

--- little_heart_bot_3/start.sh
+++ little_heart_bot_3/start.sh
@@ -1,3 +1,3 @@
 mkdir logs
-nohup node ../bilibili-pcheartbeat/app.js >> /dev/null &
+nohup node ../../bilibili-pcheartbeat/app.js >> /dev/null &
 nohup dotnet run --configuration Release >> logs/exception.txt &
