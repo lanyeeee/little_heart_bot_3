f654f6d574c4bf97de8b5f05be2f4f62751a4142
lanyeeee <lanyeeeeee@gmail.com>
2024-01-06 10:42:23 +0800

    feat: 将App的一些功能写成AppService

--- little_heart_bot_3/src/Program.cs
+++ little_heart_bot_3/src/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.Text.Encodings.Web;
 using System.Text.Json;
+using little_heart_bot_3.Data;
 using little_heart_bot_3.Services;
+using little_heart_bot_3.Services.Implements;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -15,6 +18,7 @@ public class Program
     {
         var builder = Host.CreateApplicationBuilder(args);
 
+        builder.Services.AddDbContext<LittleHeartDbContext>();
         builder.Services.AddSingleton<HttpClient>();
         builder.Services.AddSingleton<JsonSerializerOptions>(_ => new JsonSerializerOptions
         {
@@ -48,9 +52,9 @@ public class Program
             .Enrich.WithCaller(true)
             .CreateLogger());
 
-//BotService
-        builder.Services.AddKeyedScoped<IBotService, Services.Implements.Bot.BotService>("bot:BotService");
-        builder.Services.AddKeyedScoped<IBotService, Services.Implements.App.BotService>("app:BotService");
+        builder.Services.AddScoped<IBotService, BotService>();
+        builder.Services.AddScoped<IAppService, AppService>();
+
 //MessageService
         builder.Services.AddKeyedScoped<IMessageService, Services.Implements.Bot.MessageService>("bot:MessageService");
         builder.Services.AddKeyedScoped<IMessageService, Services.Implements.App.MessageService>("app:MessageService");
@@ -68,73 +72,3 @@ public class Program
         host.Run();
     }
 }
-
-
-// public static class Program
-// {
-//     public static async Task Main(string[] args)
-//     {
-//         var provider = ConfigService();
-//         App app = provider.GetRequiredService<App>();
-//         Bot bot = provider.GetRequiredService<Bot>();
-//         Console.WriteLine("Running...");
-//         await Task.WhenAll(app.Main(), bot.Main());
-//     }
-//
-//     public static ServiceProvider ConfigService()
-//     {
-//         var services = new ServiceCollection();
-//
-//         services.AddSingleton<HttpClient>();
-//         services.AddSingleton<JsonSerializerOptions>(_ => new JsonSerializerOptions
-//         {
-//             Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-//         });
-//
-//         var logFormatter = new ExpressionTemplate("{ {ts:@t, template:@mt, msg:@m, level:@l, ex:@x, p:{..@p}} }\n");
-//         services.AddKeyedSingleton<ILogger>("bot:Logger", (_, _) => new LoggerConfiguration()
-//             .MinimumLevel.Debug()
-//             .WriteTo.File(
-//                 path: "logs/bot/bot-.txt",
-//                 rollingInterval: RollingInterval.Day,
-//                 fileSizeLimitBytes: 1 * 1024 * 1024,
-//                 rollOnFileSizeLimit: true,
-//                 buffered: true,
-//                 flushToDiskInterval: TimeSpan.FromSeconds(10),
-//                 formatter: logFormatter)
-//             .Enrich.WithCaller(true)
-//             .CreateLogger());
-//
-//         services.AddKeyedSingleton<ILogger>("app:Logger", (_, _) => new LoggerConfiguration()
-//             .MinimumLevel.Debug()
-//             .WriteTo.File(
-//                 path: "logs/app/app-.txt",
-//                 rollingInterval: RollingInterval.Day,
-//                 fileSizeLimitBytes: 2 * 1024 * 1024,
-//                 rollOnFileSizeLimit: true,
-//                 buffered: true,
-//                 flushToDiskInterval: TimeSpan.FromSeconds(1),
-//                 formatter: logFormatter)
-//             .Enrich.WithCaller(true)
-//             .CreateLogger());
-//
-//         //BotService
-//         services.AddKeyedScoped<IBotService, Services.Implements.Bot.BotService>("bot:BotService");
-//         services.AddKeyedScoped<IBotService, Services.Implements.App.BotService>("app:BotService");
-//         //MessageService
-//         services.AddKeyedScoped<IMessageService, Services.Implements.Bot.MessageService>("bot:MessageService");
-//         services.AddKeyedScoped<IMessageService, Services.Implements.App.MessageService>("app:MessageService");
-//         //TargetService
-//         services.AddKeyedScoped<ITargetService, Services.Implements.Bot.TargetService>("bot:TargetService");
-//         services.AddKeyedScoped<ITargetService, Services.Implements.App.TargetService>("app:TargetService");
-//         //UserService
-//         services.AddKeyedScoped<IUserService, Services.Implements.Bot.UserService>("bot:UserService");
-//         services.AddKeyedScoped<IUserService, Services.Implements.App.UserService>("app:UserService");
-//
-//         services.AddSingleton<Bot>();
-//         services.AddSingleton<App>();
-//
-//         services.AddSingleton<IServiceProvider>(provider => provider);
-//         return services.BuildServiceProvider();
-//     }
-// }
---
+++ little_heart_bot_3/src/Services/Implements/AppService.cs
@@ -0,0 +1,155 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using little_heart_bot_3.Data;
+using little_heart_bot_3.Data.Models;
+using little_heart_bot_3.Others;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Retry;
+using Serilog;
+
+namespace little_heart_bot_3.Services.Implements;
+
+public class AppService : IAppService
+{
+    private readonly ILogger _logger;
+    private readonly JsonSerializerOptions _options;
+    private readonly HttpClient _httpClient;
+
+    private readonly ResiliencePipeline _verifyCookiesPipeline;
+
+    public AppService(
+        [FromKeyedServices("app:Logger")] ILogger logger,
+        JsonSerializerOptions options,
+        HttpClient httpClient)
+    {
+        _logger = logger;
+        _options = options;
+        _httpClient = httpClient;
+
+        _verifyCookiesPipeline = new ResiliencePipelineBuilder()
+            .AddRetry(new RetryStrategyOptions
+            {
+                ShouldHandle = new PredicateBuilder()
+                    .Handle<LittleHeartException>(ex => ex.Reason == Reason.NullResponse)
+                    .Handle<HttpRequestException>(),
+                Delay = TimeSpan.FromSeconds(1),
+                BackoffType = DelayBackoffType.Exponential,
+                MaxRetryAttempts = 5,
+                OnRetry = args =>
+                {
+                    var user = args.Context.Properties.GetValue(LittleHeartResilienceKeys.User, null)!;
+                    _logger.Warning(args.Outcome.Exception,
+                        "uid {Uid} 验证cookie时遇到异常，准备在 {RetryDelay} 秒后进行第 {AttemptNumber} 次重试",
+                        user.Uid,
+                        args.RetryDelay.TotalSeconds,
+                        args.AttemptNumber);
+                    return default;
+                }
+            })
+            .Build();
+    }
+
+    public async Task VerifyCookiesAsync(CancellationToken cancellationToken)
+    {
+        await using var db = new LittleHeartDbContext();
+        List<UserModel> users = await db.Users
+            .Include(u => u.Messages)
+            .Include(u => u.Targets)
+            .AsSplitQuery()
+            .Where(u => u.CookieStatus == CookieStatus.Unverified)
+            .ToListAsync(cancellationToken);
+        foreach (var user in users)
+        {
+            var context = ResilienceContextPool.Shared.Get(cancellationToken);
+            context.Properties.Set(LittleHeartResilienceKeys.User, user);
+
+            try
+            {
+                await _verifyCookiesPipeline.ExecuteAsync(async ctx =>
+                {
+                    HttpResponseMessage responseMessage = await _httpClient.SendAsync(new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Get,
+                        RequestUri = new Uri("https://api.bilibili.com/x/web-interface/nav"),
+                        Headers = { { "Cookie", user.Cookie } }
+                    }, ctx.CancellationToken);
+                    await Task.Delay(1000, ctx.CancellationToken);
+
+                    JsonNode response =
+                        await responseMessage.Content.ReadFromJsonAsync<JsonNode>(_options,
+                            ctx.CancellationToken) ??
+                        throw new LittleHeartException(Reason.NullResponse);
+
+                    int? code = (int?)response["code"];
+                    if (code == 0)
+                    {
+                        user.CookieStatus = CookieStatus.Normal;
+                        await db.SaveChangesAsync(CancellationToken.None);
+                    }
+                    else if (code == -412)
+                    {
+                        _logger.ForContext("Response", response.ToJsonString(_options))
+                            .Warning("uid {Uid} 验证cookie的请求被拦截", user.Uid);
+                        throw new LittleHeartException(Reason.Ban);
+                    }
+                    else if (code == -101)
+                    {
+                        _logger.ForContext("Response", response.ToJsonString(_options))
+                            .Information("uid {uid} 提供的cookie错误或已过期", user.Uid);
+                        throw new LittleHeartException(Reason.CookieExpired);
+                    }
+                    else
+                    {
+                        _logger.ForContext("Response", response.ToJsonString(_options))
+                            .Error("uid {uid} 验证cookie时出现预料之外的错误", user.Uid);
+                        user.CookieStatus = CookieStatus.Error;
+                        await db.SaveChangesAsync(CancellationToken.None);
+                    }
+                }, context);
+            }
+            catch (LittleHeartException ex)
+            {
+                switch (ex.Reason)
+                {
+                    case Reason.Ban:
+                        throw;
+                    case Reason.CookieExpired:
+                        user.CookieStatus = CookieStatus.Error;
+                        await db.SaveChangesAsync(CancellationToken.None);
+                        break;
+                    case Reason.NullResponse:
+                        _logger.Error(ex,
+                            "uid {Uid} 验证cookie时出现 NullResponse 异常，polly尝试多次后依旧发生异常",
+                            user.Uid);
+                        user.CookieStatus = CookieStatus.Error;
+                        await db.SaveChangesAsync(CancellationToken.None);
+                        break;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.Error(ex,
+                    "uid {Uid} 验证cookie时出现 HttpRequestException 异常，polly尝试多次后依旧发生异常",
+                    user.Uid);
+                user.CookieStatus = CookieStatus.Error;
+                await db.SaveChangesAsync(CancellationToken.None);
+                throw new LittleHeartException(Reason.Ban);
+            }
+            finally
+            {
+                ResilienceContextPool.Shared.Return(context);
+            }
+        }
+    }
+
+    public async Task SendMessageAsync(List<UserModel> users, CancellationToken cancellationToken)
+    {
+    }
+
+    public async Task WatchLiveAsync(List<UserModel> users, CancellationToken cancellationToken)
+    {
+    }
+}
